93cb65faaa3eef9298a74218f0eca975d303476a
abhinavverma2000 <abhinavverma2024@gmail.com>
2023-09-13 15:14:14 +0530

    first commit

---
+++ src/assets/README.md
@@ -0,0 +1,3 @@
+# angularecommercegifyshop
+Angular 16 Gift Shop Ecommerce Website Template Free<br>
+Guys here is the live demo and code snippet link : <a href="https://therichpost.com/angular-16-gift-shop-ecommerce-website-template-free/">Angular 16 Gift Shop Ecommerce Website Template Free<</a>
---
+++ src/assets/js/back-to-top.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+  setTimeout(function(){
+  // Catch the HTML element
+  const backToTop = document.querySelector('#back-to-top');
+
+  // Show and hide scroll button
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 700) {
+      backToTop.style.display = 'flex';
+    } else {
+      backToTop.style.display = 'none';
+    }
+  });
+
+  // add functionality to scroll button
+  backToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+  });
+}, 2000);
+})();
